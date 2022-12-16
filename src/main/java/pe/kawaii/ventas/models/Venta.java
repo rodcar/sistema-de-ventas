df03ebd00858047020a6126b5b2812762f5531d5
rodcar <ivanrod12@hotmail.com>
2022-12-15 22:29:24 -0500

    agrega DAO de ventas

---
+++ src/main/java/pe/kawaii/ventas/models/Venta.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.kawaii.ventas.models;
+
+/**
+ *
+ * @author Ivan
+ */
+public class Venta {
+
+    private int id;
+
+    public Venta() {
+    }
+
+    public int getId() {
+        return this.id;
+    }
+}
