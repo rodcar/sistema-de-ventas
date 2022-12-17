2275b64b1a2262db4fb24dc793d221291ea96aed
rodcar <ivanrod12@hotmail.com>
2022-12-16 21:07:49 -0500

    agrega el modelo de Venta

--- src/main/java/pe/kawaii/ventas/models/Venta.java
+++ src/main/java/pe/kawaii/ventas/models/Venta.java
@@ -4,6 +4,9 @@
  */
 package pe.kawaii.ventas.models;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  *
  * @author Ivan
@@ -11,11 +14,75 @@ package pe.kawaii.ventas.models;
 public class Venta {
 
     private int id;
+    private Usuario vendedor;
+    private Cliente cliente;
+    private ArrayList<VentaDetalle> detalles;
+    private Date fechaRegistro;
+    private double total;
 
     public Venta() {
     }
 
-    public int getId() {
-        return this.id;
+    public Venta(int id, Usuario vendedor, Cliente cliente, ArrayList<VentaDetalle> detalles, Date fechaRegistro, double total) {
+        this.id = id;
+        this.vendedor = vendedor;
+        this.cliente = cliente;
+        this.detalles = detalles;
+        this.fechaRegistro = fechaRegistro;
+        this.total = total;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Usuario getVendedor() {
+        return vendedor;
+    }
+
+    public void setVendedor(Usuario vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public ArrayList<VentaDetalle> getDetalles() {
+        return detalles;
+    }
+
+    public void setDetalles(ArrayList<VentaDetalle> detalles) {
+        this.detalles = detalles;
+    }
+
+    public Date getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "Venta{" + "id=" + id + ", vendedor=" + vendedor + ", cliente=" + cliente + ", detalles=" + detalles + ", fechaRegistro=" + fechaRegistro + ", total=" + total + '}';
+    }
+
 }
