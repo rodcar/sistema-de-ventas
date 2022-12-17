10145aa244dd480c3dfb010a3373377e93aad64d
rodcar <ivanrod12@hotmail.com>
2022-12-16 21:09:53 -0500

    agrega interfaz CrudService

---
+++ src/main/java/pe/kawaii/ventas/services/CrudService.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package pe.kawaii.ventas.services;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ *
+ * @author Ivan
+ */
+public interface CrudService<T> {
+
+    /**
+     * Registra el objeto
+     *
+     * @param t Objeto
+     */
+    void save(T t);
+
+    /**
+     * Actualiza el objeto
+     *
+     * @param t Objeto
+     */
+    void update(T t);
+
+    /**
+     * Busca un objeto según su id
+     *
+     * @param id id del objeto
+     * @return Objeto de tipo <T> según su id
+     */
+    Optional<T> findById(int id);
+
+    /**
+     * Busca todos los objetos
+     *
+     * @return Lista de todos los objetos
+     */
+    Optional<List<T>> findAll();
+
+    /**
+     * Elimina un objeto según su id
+     *
+     * @param id id del objeto
+     */
+    void delete(int id);
+}
