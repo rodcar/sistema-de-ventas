90216dc91d1652e5bc670c088b197aaef67ac954
rodcar <ivanrod12@hotmail.com>
2022-12-18 16:10:10 -0500

    agrega entidad Usuario

--- src/main/java/pe/kawaii/ventas/models/Usuario.java
+++ src/main/java/pe/kawaii/ventas/models/Usuario.java
@@ -9,5 +9,42 @@ package pe.kawaii.ventas.models;
  * @author Ivan
  */
 public class Usuario {
-    
+
+    private String username;
+    private String password;
+    private Rol rol;
+
+    public Usuario() {
+    }
+
+    public Usuario(String username, String password, Rol rol) {
+        this.username = username;
+        this.password = password;
+        this.rol = rol;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+ 
 }
