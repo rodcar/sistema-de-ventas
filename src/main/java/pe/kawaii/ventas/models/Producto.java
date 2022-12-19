b287e4a2768ad9a5951e8afa4d4874dd1f321832
rodcar <ivanrod12@hotmail.com>
2022-12-18 20:06:27 -0500

    actualiza clase Producto

--- src/main/java/pe/kawaii/ventas/models/Producto.java
+++ src/main/java/pe/kawaii/ventas/models/Producto.java
@@ -6,13 +6,33 @@ package pe.kawaii.ventas.models;
 
 /**
  *
- * @author EDITA
+ * @author Ivan
  */
 public class Producto {
-     private String nombre;
 
-    public Producto(String nombre) {
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+    private int stock;
+
+    public Producto() {
+    }
+
+    public Producto(int id, String nombre, String descripcion, double precio, int stock) {
+        this.id = id;
         this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getNombre() {
@@ -22,6 +42,29 @@ public class Producto {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    
-    
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
 }
