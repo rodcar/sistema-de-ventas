3c96d20d4fb33ec1dbc16b6c419bb618da07bb2d
rodcar <ivanrod12@hotmail.com>
2022-12-20 16:43:03 -0500

    agrega paneles de mantenimiento

--- src/main/java/pe/kawaii/ventas/views/productos/PanelActualizarProducto.java
+++ src/main/java/pe/kawaii/ventas/views/productos/PanelActualizarProducto.java
@@ -2,18 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
  */
-package pe.kawaii.ventas.views;
+package pe.kawaii.ventas.views.productos;
 
 /**
  *
  * @author Ivan
  */
-public class PanelVendedor extends javax.swing.JPanel {
+public class PanelActualizarProducto extends javax.swing.JPanel {
 
     /**
-     * Creates new form PanelVendedor
+     * Creates new form PanelActualizarProductos
      */
-    public PanelVendedor() {
+    public PanelActualizarProducto() {
         initComponents();
     }
 
